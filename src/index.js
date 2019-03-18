74f2674ed41c71a494881f3d024585f03e732d66
Ange <angelina.derman@mail.ru>
2019-03-18 22:25:36 +0300

    feat:just

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let i=0;
+   let iteration=str.length;
+   let firstBracket;
+   let secondBracket;
+   while(i<iteration){
+    for( let i=0; i<=bracketsConfig.length-1;i++)
+    {
+    firstBracket=bracketsConfig[i][0];
+    secondBracket=bracketsConfig[i][1];
+    SumBracket=firstBracket+secondBracket;
+    str=str.replace(SumBracket,"");
+
+};
+   if(str=="")
+   {return true;};
+   i++;}
+    if(str=="")
+    {
+    return true;
+    }
+    else{ 
+    return false;
+    }
 }
+
+
